25e29fe3a68265fd5f36729f8803bdf3165f26ba
Dmytro Iudin <dmtry.iudin@gmail.com>
2024-09-14 21:25:19 +0300

    test login form

---
+++ client/src/app/auth/layout.tsx
@@ -0,0 +1,22 @@
+import { Box, Center, Container } from "@chakra-ui/react";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Container maxW="100%" bg="cyan.700">
+      <Center minH="100vh" py="4">
+        <Box
+          bg="white"
+          rounded="lg"
+          p="5"
+          w={{ base: "100%", lg: "75%", xl: "50%" }}
+        >
+          {children}
+        </Box>
+      </Center>
+    </Container>
+  );
+}
